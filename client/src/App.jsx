41170087edb0c8be3a82e3cdc8b44e9d54c8897f
Juyeon Lee <lqu134@naver.com>
2025-06-25 18:54:39 +0900

    feat: create Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </div>
   )
 }
 
