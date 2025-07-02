a2a48c44939f8be4e27a03f079b6b5e3469647a3
Juyeon Lee <jl@Juyeons-MacBook-Pro.local>
2025-07-02 18:03:40 +0900

    feat:add comment section with input form

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,22 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { blog_data } from "../assets/assets";
+import { blog_data, comments_data } from "../assets/assets";
 import Navbar from "../components/Navbar";
 import Moment from "moment";
+import Footer from "../components/Footer";
 
 function Blog() {
   const { id } = useParams();
 
   const [data, setData] = useState(null);
+  const [comments, setComments] = useState([]);
 
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
 
   const fetchBlogData = async () => {
     const data = blog_data.find((item) => item._id === id);
     setData(data);
   };
 
+  const fetchComments = async () => {
+    setComments(comments_data);
+  };
+
+  const addComment = (e) => {
+    e.preventDefault();
+  };
+
   useEffect(() => {
     fetchBlogData();
+    fetchComments();
   }, []);
 
   return data ? (
@@ -43,7 +56,39 @@ function Blog() {
           dangerouslySetInnerHTML={{ __html: data.description }}
         ></div>
 
+        {/* Comments section */}
+        <div className="mt-14 mb-10 max-w-lg mx-auto">
+          <p className="font-semibold mb-4">Comments({comments.length})</p>
+          <div className="flex flex-col gap-4">
+            {comments.map((item, index) => (
+              <div
+                key={index}
+                className="relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600"
+              >
+                <div className="mb-2">
+                  <p className="font-medium">{item.name}</p>
+                </div>
+                <p className="text-sm max-w-md">{item.content}</p>
+                <div className="absolute right-4 bottom-3 flex items-center gap-2 text-xs">
+                  {Moment(item.createdAt).fromNow()}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* Add comment section*/}
+        <div className="max-w-lg mx-auto">
+          <p className="font-semibold mb-4">Add your comment</p>
+          <form onSubmit={addComment} className="flex flex-col items-center gap-4 max-w-lg">
+            <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder="Name" required className="w-full p-2 border border-gray-300 rounded outline-none" />
+
+            <textarea onChange={(e) => setContent(e.target.value)} value={content} placeholder="Comment" className="w-full p-2 border border-gray-300 rounded outline-none h-48" required></textarea>
+
+            <button type="sumbit" className="bg-primary text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer">Submit</button>
+          </form>
+        </div>
       </div>
+      <Footer />
     </div>
   ) : (
     <div>Loading...</div>
