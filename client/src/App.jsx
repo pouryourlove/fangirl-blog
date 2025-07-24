48b89e23696fc6f8886e96fb904f06cbb9d38aef
Juyeon Lee <lqu134@naver.com>
2025-07-24 16:07:37 +0900

    feat: add AddBlog page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import ListBlog from './pages/admin/ListBlog'
 import AddBlog from './pages/admin/AddBlog'
 import Comments from './pages/admin/Comments'
 import Login from './pages/admin/Login'
+import 'quill/dist/quill.snow.css'
 
 function App() {
   return (
