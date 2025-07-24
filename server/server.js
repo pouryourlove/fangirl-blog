e29c4880f411327be61913f8455fc08e8cb70080
Juyeon Lee <lqu134@naver.com>
2025-07-24 17:34:33 +0900

    feat: setup server and MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import connectDB from './configs/db.js';
+
+const app = express();
+
+await connectDB()
+
+// Middlewares
+
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.get('/', (req, res)=> res.send("API is working"))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log('server is running on port' + PORT)
+})
+
+export default app;
