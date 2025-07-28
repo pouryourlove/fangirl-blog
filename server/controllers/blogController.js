0a689dafb0b215309559527c2115bd1b7ebd128f
Juyeon Lee <lqu134@naver.com>
2025-07-28 15:37:36 +0900

    feat: upload blog images to ImageKit and save blog data to MongoDB

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,3 +1,7 @@
+import fs from 'fs'
+import imagekit from '../configs/imageKit.js';
+import Blog from '../models/Blog.js';
+
 export const addBlog = async (req, res) => {
   try {
     const { title, subTitle, description, category, isPublished } = JSON.parse(
@@ -9,5 +13,34 @@ export const addBlog = async (req, res) => {
     if(!title||!description || !category || !imageFile){
         return res.json({success: false, message: "missing required feilds"})
     }
-  } catch (error) {}
+
+    const fileBuffer = fs.readFileSync(imageFile.path)
+
+    // upload image to imagekit
+    const response = await imagekit.upload({
+        file:fileBuffer,
+        fileName: imageFile.originalname,
+        folder: "/blogs"
+    })
+
+    // optimization through imagekit url transformation
+    const optimizedImageUrl = imagekit.url({
+        path: response.filePath,
+        transformation: [
+            {quality: 'auto'}, //auto compression
+            {format: 'webp'}, //convert to modern format
+            {width: '1280'} // width resizing
+        ]
+    });
+
+    const image = optimizedImageUrl;
+
+    await Blog.create({title, subTitle, description, category, image, isPublished})
+
+    res.json({success: true, message: "blog added successfully"})
+
+
+  } catch (error) {
+    res.json({success: false, message: error.message})
+  }
 };
